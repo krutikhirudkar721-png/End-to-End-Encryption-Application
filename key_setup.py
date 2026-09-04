efa7cb60b6bdaaf5be6ccac9317e6ab0fc7ffa62
Krutik Hirudkar <krutikhirudkar721@gmail.com>
2026-09-04 22:22:37 +0530

    Update key_setup.py

--- key_setup.py
+++ key_setup.py
@@ -1,8 +1,5 @@
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
-
 key = AESGCM.generate_key(bit_length=256)
-
 with open("shared_key.bin", "wb") as file:
     file.write(key)
-
 print("Secret key generated successfully.")
